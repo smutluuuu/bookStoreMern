4faad312ee1752af1fd50a4a5a781afc9d6bbe5e
Suleyman Mutlu <suleyman.mutlu96@gmail.com>
2024-03-14 18:19:12 +0300

    mongodb password default

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 export const PORT = 5555;
 
 export const mongoDBURL =
-  'mongodb+srv://suleyman:123@cluster0.e7fwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+  'mongodb+srv://suleyman:<password>@cluster0.e7fwn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
